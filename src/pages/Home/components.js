4a63c239c43fd2e831be7cd4d9c1800ececc0a70
therealdrtroll <ar1pperk@gmail.com>
2021-05-28 18:28:45 +0530

    search box layout complete

--- src/pages/Home/components.js
+++ src/pages/Home/components.js
@@ -6,6 +6,7 @@ const Wrapper = styled.div`
   width: 100%;
   max-width: 500px;
   padding: 10px;
+  min-height: 600px; //stops search box from moving
 `;
 
 const Logo = styled.div`
@@ -19,22 +20,23 @@ const Logo = styled.div`
 const SearchBox = styled.div`
   width: 100%;
   border-radius: 25px;
-  padding: 10px 5px;
+  padding: 5px 0px;
   display: flex;
   flex-direction: column;
-  background: #2d2f30;
-  > div {
-  }
+  background: ${(props) => props.theme.backgroundSecondary};
 `;
 
 const SearchHead = styled.div`
   width: 95%;
   margin: 0 auto;
   border-radius: 25px;
+  padding: 0px 5px;
+
   display: flex;
   align-items: center;
   flex-wrap: wrap;
   gap: 10px;
+
   > * {
     display: flex;
     justify-content: center;
@@ -46,33 +48,60 @@ const SearchInput = styled.input`
   border: 0;
   outline: 0;
   font-size: 16px;
-  background: #2d2f30;
-  color: #f2f2f2;
+  background: inherit;
+  color: ${(props) => props.theme.fontPrimary};
   flex-grow: 8;
+  text-transform: capitalize;
 `;
 
 const SearchDivider = styled.div`
   height: ${(props) => (props.direction === "vertical" ? "35px" : "1px")};
-  width: ${(props) => (props.direction === "vertical" ? "1px" : "95%")};
+  width: ${(props) => (props.direction === "vertical" ? "1px" : "98%")};
   margin: 3px auto;
-  background-color: #110b0b;
+  background: ${(props) => props.theme.backgroundPrimary};
 `;
 
 const SearchIcon = styled.img`
   flex: 1;
-  background-color: ${(props) => props.background};
+  background: ${(props) =>
+    props.background ? props.theme.backgroundTertiary : "inherit"};
   border-radius: 50%;
   padding: 5px;
   max-width: 25px;
 `;
 
 const SearchBody = styled.div`
-  min-height: 100px;
-  /* display: none; */
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 10px;
 `;
 
 const SearchCard = styled.div`
-  border: 1px solid white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  color: ${(props) => props.theme.fontPrimary};
+  padding: 5px 15px;
+  margin: 5px 0;
+  cursor: pointer;
+
+  p {
+    font-size: 16px;
+    font-weight: 500;
+    margin: 0;
+  }
+
+  span {
+    font-size: 14px;
+    text-transform: capitalize;
+    margin: 0;
+  }
+
+  :hover {
+    background: ${(props) => props.theme.backgroundTertiary};
+    color: ${(props) => props.theme.fontTertiary};
+  }
 `;
 
 export {
@@ -84,4 +113,5 @@ export {
   SearchBox,
   SearchDivider,
   SearchBody,
+  SearchCard,
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Wrapper,
   Logo,
@@ -8,13 +8,30 @@ import {
   SearchDivider,
   SearchHead,
   SearchBody,
+  SearchCard,
 } from "./components";
 import logo from "./resources/star-wars-logo.png";
 import cancel from "./resources/cancel.svg";
 import spinner from "./resources/spinner.svg";
 import search from "./resources/search.svg";
+import FetchData from "../../hooks/fetchData";
 
 function HomePage() {
+  const [query, setQuery] = useState("");
+
+  const { data, isLoading, isError } = FetchData(
+    `https://swapi.dev/api/people/`,
+    query
+  );
+
+  useEffect(() => {
+    console.log(data);
+  }, [query]);
+
+  const onQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
     <Wrapper>
       <Logo>
@@ -24,19 +41,19 @@ function HomePage() {
       <SearchBox>
         {/* Search Head Start */}
         <SearchHead>
-          <SearchInput placeholder="Search by name" />
+          <SearchInput
+            onChange={onQueryChange}
+            value={query}
+            placeholder="Search by name"
+          />
 
-          <SearchIcon
-            background="inherit"
-            src={cancel}
-            alt="clear"
-          ></SearchIcon>
+          <SearchIcon background={false} src={cancel} alt="cancel"></SearchIcon>
 
           <SearchDivider direction="vertical" />
 
           <SearchIcon
-            background="#ffeb00"
-            src={search}
+            background={isLoading ? false : true}
+            src={isLoading ? spinner : search}
             alt="clear"
           ></SearchIcon>
         </SearchHead>
@@ -44,7 +61,19 @@ function HomePage() {
         <SearchDivider direction="horizontal" />
 
         {/* Search Body Start */}
-        <SearchBody></SearchBody>
+        <SearchBody>
+          {data?.map(({ birth_year, gender, name, url }) => (
+            <SearchCard key={url}>
+              <div>
+                <p>{name}</p>
+                <span>{birth_year}</span>
+              </div>
+              <div>
+                <span>{gender}</span>
+              </div>
+            </SearchCard>
+          ))}
+        </SearchBody>
       </SearchBox>
     </Wrapper>
   );
