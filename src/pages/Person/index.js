1e312ba2f2efacbd9980118783af259a3f61b85f
therealdrtroll <ar1pperk@gmail.com>
2021-05-28 23:33:02 +0530

    file structure change

--- src/pages/Person/index.js
+++ src/pages/Person/index.js
@@ -1,10 +1,37 @@
-import React from "react";
-import "./index.css";
+import React, { useEffect } from "react";
+import { useLocation, useParams } from "react-router";
+import useFetchPerson from "./useFetchPerson";
+import scene from "./Resources/scene.svg";
 
 function Person() {
+  const { id } = useParams();
+
+  const { data, isLoading, isError } = useFetchPerson(
+    `https://swapi.dev/api/people/${id}/`
+  );
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const {
+    name,
+    birth_year,
+    eye_color,
+    gender,
+    hair_color,
+    height,
+    mass,
+    skin_color,
+  } = data;
+
   return (
     <div className="person">
-      <h1>Luke Skywalker</h1>
+      <h1>{name}</h1>
+      <h1>{birth_year}</h1>
+      <h1>{eye_color}</h1>
+      <h1>{gender}</h1>
+      <h1>{hair_color}</h1>
     </div>
   );
 }
