3fd6308d7e853bbd8c9c11cb275ed1c91c58c379
therealdrtroll <ar1pperk@gmail.com>
2021-05-28 18:24:25 +0530

    added theme provider

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Person from "./pages/Person";
 import HomePage from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
-import "./App.css";
+const Wrapper = styled.div`
+  min-height: 100vh;
+  background: #110b0b;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
+  const theme = {
+    fontPrimary: "#9fa0a1",
+    fontSecondary: "#ffeb00",
+    fontTertiary: "#2D2F30",
+
+    backgroundPrimary: "#110B0B",
+    backgroundSecondary: "#2D2F30",
+    backgroundTertiary: "#ffeb00",
+  };
+
   return (
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
 
-          <Route path="/person/:id">
-            <Person />
-          </Route>
+            <Route path="/person/:id">
+              <Person />
+            </Route>
 
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
